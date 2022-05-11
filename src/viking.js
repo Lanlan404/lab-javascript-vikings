a6083b8092e94bb39e2521fdffbbb19b9af56020
Cécile Deshayes-Guinard <cecile.deshayes.guinard@gmail.com>
2022-05-11 19:37:02 +0200

    done

--- src/viking.js
+++ src/viking.js
@@ -2,7 +2,7 @@
 class Soldier {
   constructor(health, strength) {
     this.health= health;
-    this.strength= strength
+    this.strength= strength;
   }
   attack(){
     return this.strength;
@@ -13,14 +13,49 @@ class Soldier {
 }
 
 // Viking
-class Viking {}
+
+
+  class Viking extends Soldier {
+    constructor(name, health, strength) {
+      super (health, strength);  
+      this.name= name;
+        
+    }
+    attack() {
+      super.attack()
+      return this.strength;
+   }
+
+    receiveDamage (damage) {
+       this.health -= damage;
+
+       if (this.health > 0) {
+         return `${this.name} has received ${damage} points of damage`
+       }
+
+       if (this.health <= 0) {
+         return `${this.name} has died in act of combat`
+       }
+    }
+
+    battleCry () {
+       return `Odin Owns You All!`
+    }
+
+  }
+
+
+
 
 // Saxon
-class Saxon {}
+class Saxon {
+
+
+  
+}
 
 // War
 class War {}
-
 
 
 // The following is required to make unit tests work.
